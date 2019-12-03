bd124aae65449ea7288c6dae08b2d8d850056520
Martin Kruger <martin@martinkruger.com>
2019-12-04 00:10:30 +0200

    Blog content loaded

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../Components/Layout/Layout';
+
+//
+// Goal: Link to the blog posts
+//
+// 1. Fetch the slug for posts
+// 2. Use slug to generate a link to the post page
+// 3. Test your work!
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -14,6 +21,9 @@ const BlogPage = () => {
               date
             }
             id
+            fields {
+              slug
+            }
           }
         }
       }
@@ -25,8 +35,10 @@ const BlogPage = () => {
       <ol>
         {data.allMarkdownRemark.edges.map(edge => (
           <li key={edge.node.id}>
-            <h2>{edge.node.frontmatter.title}</h2>
-            <p>{edge.node.frontmatter.date}</p>
+            <Link to={`/blog/${edge.node.fields.slug}`}>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </Link>
           </li>
         ))}
       </ol>
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import DOMPurify from 'dompurify';
+
 import Layout from '../Components/Layout/Layout';
 
-const Blog = () => {
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`;
+
+const Blog = props => {
+  const htmlContent = props.data.markdownRemark.html;
+  const sanitize = DOMPurify.sanitize;
   return (
     <Layout>
-      <h1>This is the blog template</h1>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: sanitize(htmlContent) }}></div>
     </Layout>
   );
 };
